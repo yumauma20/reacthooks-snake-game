d7eead187deb5f81fb6b105cb560539f09655b4b
yumauma20 <71yuma10@gmail.com>
2021-03-15 23:29:21 +0900

    フレームの作成

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import Navigation from './components/Navigation';
+import Field from './components/Field';
+import Button from './components/Button';
+import ManipulationPanel from './components/ManipulationPanel';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <header className="header">
+        <div className="title-container">
+          <h1 className="title">Snake Game</h1>
+        </div>
+        <Navigation />
+      </header>
+      <main className="main">
+        <Field />
+      </main>
+      <footer className="footer">
+        <Button />
+        <ManipulationPanel />
+      </footer>
     </div>
   );
 }
