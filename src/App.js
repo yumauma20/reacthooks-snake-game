014acf6589243cbaa3b0d4423826cf5a09755b12
yumauma20 <71yuma10@gmail.com>
2021-03-16 22:59:20 +0900

    進むボタンを押した時に上に動くように実装

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Navigation from './components/Navigation';
 import Field from './components/Field';
 import Button from './components/Button';
 import ManipulationPanel from './components/ManipulationPanel';
 import { initFields } from './utils';
 
-const fields = initFields(35);
+const initialPosition = {x : 17, y : 17};
+const initialValues = initFields(35, initialPosition);
 
 function App() {
+  const [fields, setFields] = useState(initialValues);
+  const [position, setPosition] = useState();
+
+  useEffect(() => {
+    setPosition(initialPosition);
+  },[]);
+
+  const goUp = () => {
+    const { x, y } = position
+    const nextY = Math.max(y-1, 0) /* y-1と0のどちらか大きい方をnextYに代入 */
+    fields[y][x] = ""
+    fields[nextY][x] = 'snake'
+    setPosition({x, y:nextY})
+    setFields(fields)
+  }
+
   return (
     <div className="App">
       <header className="header">
@@ -19,6 +36,9 @@ function App() {
       <main className="main">
         <Field fields={fields}/>
       </main>
+      <div style={{ padding: '16px' }}>
+        <button onClick={goUp}>進む</button>
+      </div>
       <footer className="footer">
         <Button />
         <ManipulationPanel />
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,10 +1,10 @@
-export const initFields = (fieldSize) => {
+export const initFields = (fieldSize, initialPosition) => {
   const fields = []
   for(let i = 0; i < fieldSize; i++) {
     const cols = new Array(fieldSize).fill('')
     fields.push(cols)
   }
-  fields[17][17] = 'snake';
+  fields[initialPosition.y][initialPosition.x] = 'snake';
 
   return fields
 }
