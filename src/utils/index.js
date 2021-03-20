44697986a910a235ee5c7d7ccf0ebe4886d092c0
yumauma20 <71yuma10@gmail.com>
2021-03-20 22:06:05 +0900

    餌の位置がスネークの位置とかぶらないように修正

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,18 +1,24 @@
-const getFoodPosition = (fieldSize) => {
-  const x = Math.floor(Math.random() * (fieldSize - 1 - 1)) + 1;
-  const y = Math.floor(Math.random() * (fieldSize - 1 - 1)) + 1;
-  return { x, y };
+const getFoodPosition = (fieldSize, excludes) => {
+  while(true) {
+    const x = Math.floor(Math.random() * (fieldSize - 2)) + 1;
+    const y = Math.floor(Math.random() * (fieldSize - 2)) + 1;
+    const conflict = excludes.some(item => item.x === x && item.y === y)
+
+    if(!conflict) {
+      return { x, y };
+    }
+  }
 }
 
-export const initFields = (fieldSize, initialPosition) => {
+export const initFields = (fieldSize, snake) => {
   const fields = []
   for(let i = 0; i < fieldSize; i++) {
     const cols = new Array(fieldSize).fill('')
     fields.push(cols)
   }
-  fields[initialPosition.y][initialPosition.x] = 'snake';
+  fields[snake.y][snake.x] = 'snake';
 
-  const food = getFoodPosition(fieldSize)
+  const food = getFoodPosition(fieldSize, [snake])
   fields[food.y][food.x] = 'food'
 
   return fields
