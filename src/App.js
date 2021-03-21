5f07facfd981c546adf06095e222dbeb5119f56c
yumauma20 <71yuma10@gmail.com>
2021-03-21 23:12:49 +0900

    難易度を調整できるように変更

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import { initFields, getFoodPosition } from './utils';
 const initialPosition = {x : 17, y : 17}
 const initialValues = initFields(35, initialPosition)
 const defaultInterval = 100
+const defaultDifficulty = 3
+
+const Difficulty = [1000, 500, 100, 50, 10]
 
 const GameStatus = Object.freeze({
   init: 'init',
@@ -74,17 +77,18 @@ function App() {
   const [body, setBody] = useState([])
   const [status, setStatus] = useState(GameStatus.init)
   const [direction, setDirection] = useState(Direction.up)
-  const [difficulty, setDifficulty] = useState(3)
+  const [difficulty, setDifficulty] = useState(defaultDifficulty)
   const [tick, setTick] = useState(0)
 
   useEffect(() => {
     setBody([initialPosition])
     //ゲーム中の時間を管理する
+    const interval = Difficulty[difficulty - 1]
     timer = setInterval(() => {
       setTick(tick => tick + 1)
-    }, defaultInterval)
+    }, interval)
     return unsubscribe /* return部分はコンポーネントが削除されるタイミングで実行される */
-  },[])
+  },[difficulty])
 
   //初回レンタリング時とtick(依存変数の配列)が変更されるときにレンタリングされる際に実行される
   useEffect(() => {
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
 const Navigation = ({ length, difficulty = 3, onChangeDifficulty }) => {
+  const upVisibility = difficulty < 5 ? '' :'is-hidden'
+  const downVisibility = difficulty > 1 ? '' : 'is-hidden'
+  const onUpDifficulty = () => onChangeDifficulty(difficulty + 1)
+  const onDownDifficulty = () => onChangeDifficulty(difficulty - 1)
+
   return (
     <div className="navigation">
       <div className="navigation-item">
@@ -14,6 +19,16 @@ const Navigation = ({ length, difficulty = 3, onChangeDifficulty }) => {
         <div className="navigation-item-number-container">
           <span className="num-board">{difficulty}</span>
           <div className="difficulty-button-container">
+            <div
+              className={`difficulty-button difficulty-up ${upVisibility}`}
+              onClick={onUpDifficulty}
+            >
+            </div>
+            <div
+              className={`difficulty-button difficulty-down ${downVisibility}`}
+              onClick={onDownDifficulty}
+            >
+            </div>
           </div>
         </div>
       </div>
