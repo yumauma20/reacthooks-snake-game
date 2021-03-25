9ababd9e3cb31056a589119a863df0949b76e52a
yumauma20 <71yuma10@gmail.com>
2021-03-25 23:20:00 +0900

    スタイルを整える

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 const Button = ({status, onStart, onStop, onRestart}) => {
   return (
     <div className="button">
-      { status === "gameover" && <button onClick={onRestart}>gameover</button> }
-      { status === "init" && <button onClick={onStart}>start</button> }
-      { status === "suspended" && <button onClick={onStart}>start</button> }
-      { status === "playing" && <button onClick={onStop}>stop</button> }
+      { status === "gameover" && <button className="btn btn-gameover" onClick={onRestart}>gameover</button> }
+      { status === "init" && <button className="btn btn-init" onClick={onStart}>start</button> }
+      { status === "suspended" && <button className="btn btn-suspended" onClick={onStart}>start</button> }
+      { status === "playing" && <button className="btn btn-playing" onClick={onStop}>stop</button> }
     </div>
   );
 };
--- src/components/ManipulationPanel.jsx
+++ src/components/ManipulationPanel.jsx
@@ -1,4 +1,11 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faArrowLeft,
+  faArrowUp,
+  faArrowDown,
+  faArrowRight
+} from '@fortawesome/free-solid-svg-icons'
 
 const ManipulationPanel = ({ onChange }) => {
   const onUp = () => onChange('up')
@@ -8,10 +15,20 @@ const ManipulationPanel = ({ onChange }) => {
 
   return (
     <div className="manipulation-panel">
-      <button onClick={onLeft}>←</button>
-      <button onClick={onUp}>↑</button>
-      <button onClick={onDown}>↓</button>
-      <button onClick={onRight}>→</button>
+      <button className="manipulation-btn btn btn-left" onClick={onLeft}>
+        <FontAwesomeIcon icon={faArrowLeft} />
+      </button>      
+      <div>
+        <button className="manipulation-btn btn btn-up" onClick={onUp}>
+          <FontAwesomeIcon icon={faArrowUp} />
+        </button>
+        <button className="manipulation-btn btn btn-down" onClick={onDown}>
+          <FontAwesomeIcon icon={faArrowDown} />
+        </button>
+      </div>
+      <button className="manipulation-btn btn btn-right" onClick={onRight}>
+        <FontAwesomeIcon icon={faArrowRight} />
+      </button>
     </div>
   );
 };
